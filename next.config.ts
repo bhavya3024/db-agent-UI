a76b06cba28b71337127fbf23194d131bdaa29b3
Bhavya Dhiman <bhavyadhiman7@gmail.com>
2026-03-14 19:05:05 +0530

    enable async webaseembly support

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,22 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    });
+    
+    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+    
+    return config;
+  },
 };
 
 export default nextConfig;
